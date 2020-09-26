73fd528c8268a536819f63b3f0bb3e78d1484ff4
Purukitto <purukitto@gmail.com>
2020-09-26 19:55:47 +0530

    Removed unnecessary JS

--- docs/js/custom.js
+++ docs/js/custom.js
@@ -13,21 +13,6 @@
         live: true // default
     }).init();
 
-    /* ==============================================
-    LIGHTBOX -->
-    =============================================== */
-
-    jQuery('a[data-gal]').each(function() {
-        jQuery(this).attr('rel', jQuery(this).data('gal'));
-    });
-    jQuery("a[data-rel^='prettyPhoto']").prettyPhoto({
-        animationSpeed: 'slow',
-        theme: 'light_square',
-        slideshow: true,
-        overlay_gallery: true,
-        social_tools: false,
-        deeplinking: false
-    });
 
     /* ==============================================
     SCROLL -->
@@ -60,21 +45,4 @@
         var $spy = $(this).scrollspy('refresh')
     })
 
-    /* ==============================================
-    VIDEO FIX -->
-    =============================================== */
-
-    $(document).ready(function() {
-        // Target your .container, .wrapper, .post, etc.
-        $(".media").fitVids();
-    });
-
-    /* ==============================================
-    VIDEO FIX -->
-    =============================================== */
-
-    $('.docs-sidebar>nav>li>a').click(function() {
-        $('.docs-sidebar>nav>li').removeClass('active');
-        $(this).parent().addClass('active');
-    });
 })(jQuery);
