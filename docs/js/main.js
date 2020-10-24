63a8420cb0c876e50b8a0771c07a62c5cbc94584
Purukitto <purukitto@gmail.com>
2020-10-24 20:27:19 +0530

    up

--- docs/js/main.js
+++ docs/js/main.js
@@ -19,3 +19,21 @@ function fetchAPI(input) {
     var form = document.getElementById("poke-form");
     form.reset();
 }
+
+
+function fetchFrontAPI(input) {
+    fetch('https://app.pokemon-api.xyz/' + input)
+        .then(response => response.json())
+        .then(data => {
+            var div = document.getElementById("pokeData");
+            $(div).empty();
+            poke = JSON.stringify(data, null, 4);
+            var div = document.getElementById("pokeData");
+            $(div).append(poke);
+        })
+        .catch(err => {
+            console.error(err)
+        });
+    var form = document.getElementById("poke-form");
+    form.reset();
+}
